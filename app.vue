749aedfd874f1bb2b75298433b70275212aee739
unicornikita <nikitakapusin@gmail.com>
2024-03-13 11:51:54 +0100

    removed favicon

--- app.vue
+++ app.vue
@@ -1,4 +1,3 @@
-
 <template>
   <NavigationBar></NavigationBar>
   <div class="flex flex-col items-center justify-center bg-pink-800 px-8">
@@ -24,3 +23,47 @@ body {
   font-family: "Raleway", sans-serif;
 }
 </style>
+
+<script setup lang="ts">
+useHead({
+  title: "Nikita Galuh Kapušin",
+  meta: [
+    {
+      name: "description",
+      content: "Nikita Galuh's personal portfolio"
+    },
+    {
+      name: "keywords",
+      content: "Nikita Galuh, portfolio, personal, developer, software, engineer, web, developer, front-end, programmer, mobile, flutter"
+    },
+    {
+      name: "author",
+      content: "Nikita Galuh Kapušin"
+    },
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1.0"
+    },
+    {
+      name: "robots",
+      content: "index, follow"
+    },
+    {
+      name: "og:title",
+      content: "Nikita Galuh"
+    },
+    {
+      name: "og:description",
+      content: "Nikita Galuh's personal portfolio"
+    },
+    {
+      name: "og:site_name",
+      content: "Nikita Galuh Kapušin"
+    },
+    {
+      name: "og:type",
+      content: "website"
+    },
+  ]
+});
+</script>
