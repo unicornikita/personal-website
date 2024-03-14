fdfbf3cb0821ac1049affb30a171d6c4311932d5
unicornikita <nikitakapusin@gmail.com>
2024-03-14 19:07:45 +0100

    added mail function

--- components/Contact.vue
+++ components/Contact.vue
@@ -8,20 +8,34 @@
         <form class="w-full mx-auto flex flex-col">
         <div class="mb-4">
           <label for="email" class="block mb-2 text-sm font-medium  text-white">{{ $t('email') }}</label>
-          <input type="email" id="email"
+          <input type="email" id="email" v-model="email"
             class="outline-none shadow-sm bg-pink-100 border border-white text-black text-sm rounded-lg  focus:border-pink-200 block w-full p-2.5  placeholder-gray-500"
             placeholder="name@email.com" required />
         </div>
         <div class="mb-4">
           <label for="message" class="block mb-2 text-sm font-medium text-white">{{ $t('message') }}</label>
-          <textarea id="message" rows="4"
+          <textarea id="message" rows="4" v-model="message"
             class="outline-none block p-2.5 w-full text-sm text-black bg-pink-50 rounded-lg border border-gray-300 focus:border-pink-200  placeholder-gray-500 "
             placeholder="Enter your message" required></textarea>
         </div>
 
-        <button type="submit"
+        <button type="submit" @click="sendMail"
           class="mb-6 mx-auto inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-pink-950 rounded-lg hover:bg-pink-500 focus:ring-4">
           {{ $t('messageButton') }}</button>
       </form>
     </div>
 </template>
+<script setup lang="ts">
+const { $mail } = useNuxtApp();
+
+const email = ref('');
+const message = ref('');
+
+const sendMail = () => {
+  $mail.send({
+    from: email.value,
+    subject: 'New message from your website',
+    text: `${email.value}\n${message.value}`,
+})
+}
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/i18n'],
+  modules: ['@nuxtjs/i18n',['nuxt-mail', {
+    message: {
+      to: 'nikitagaluh@kapusin.si',
+    },
+    smtp: {
+      host: "eh2.neoserv.si",
+      port: 465,
+      auth:{
+        user: "nikitagaluh@kapusin.si",
+        pass: "U!Wk6968Hn$#&M"
+      }
+    },
+  }],],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
